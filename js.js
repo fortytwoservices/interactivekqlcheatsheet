339be6f2c588c60b4b6f82184ad9f4d7df7ed8e0
remi.vandemir <remi.vandemir@amesto.no>
2023-02-12 00:00:21 +0100

    Corrected position of autocomplete
    Signed-off-by: remi.vandemir <remi.vandemir@amesto.no>

--- js.js
+++ js.js
@@ -85,7 +85,7 @@ function autocomplete(inp, arr) {
         /*create a DIV element that will contain the items (values):*/
         a = document.createElement("DIV");
         a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
+        a.setAttribute("class", "autocomplete-items container-fluid text-break");
         /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(a);
         /*for each item in the array...*/
